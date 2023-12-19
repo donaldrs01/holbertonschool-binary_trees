b487cfc9ce9805a2a7435144a60bd12daccda45c
Your Name <you@example.com>
2023-12-19 09:40:53 -0800

    measures height of each subtree and returns height +1 (to include root level)

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_height - measures height of binary tree
+ * @tree: pointer to the root node of tree
+ *
+ * Return: 0 if tree is NULL, height otherwise
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t l_height;
+	size_t r_height;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	l_height = binary_tree_height(tree->left);
+	r_height = binary_tree_height(tree->right);
+	/* traverses both subtrees and assigns height to var */
+
+	return ((l_height > r_height) ? (l_height + 1) : (r_height + 1));
+	/* checks to see if left or right side is higher */
+	/* if left, calculates left + 1. if right, calculates right + 1 */
