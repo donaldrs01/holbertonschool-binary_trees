41bc770049ced92cc337159a2840019fb19fbc2c
Your Name <you@example.com>
2023-12-20 12:50:32 -0800

    simple function to recursively measure tree size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -9,3 +9,17 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+	size_t left_size;
+	size_t right_size;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	left_size = binary_tree_size(tree->left);
+	right_size = binary_tree_size(tree->right);
+
+	return (left_size + right_size + 1);
+	/* returns combined size of left/right subtrees + 1 */
+}
