e5e113c84d82d900fd7e31b645dd3a2cffc81e50
Your Name <you@example.com>
2023-12-20 13:17:56 -0800

    calls binary_height function, calculates height of both sides, and subtracts difference

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,3 +9,13 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int l_height, r_height;
+
+	if (tree == NULL)
+		return (0);
+
+	l_height = (int)binary_tree_height(tree->left);
+	r_height = (int)binary_tree_height(tree->right);
+
+	return (l_height - r_height);
+}
