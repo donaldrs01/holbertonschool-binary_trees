9bb9480fd69899e49f35ecfce70f1a9843e922b3
Your Name <you@example.com>
2023-12-20 13:01:59 -0800

    checks for leaves on binary tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -10,3 +10,13 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	/* returns 1 if current node has no left/right children */
+
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	/* if not a leaf, recurisvely counts leaves on left/right side */
+}
