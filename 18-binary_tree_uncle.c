92b375181be982f0dc45ca2d7c56d955382e0140
Your Name <you@example.com>
2023-12-20 14:18:43 -0800

    creates grandparent var, tracks uncle of current node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	binary_tree_t *grandparent;
 
-	if (node == NULL || node->parent == NULL || grandparent == NULL)
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 	{
 		return (NULL);
 	}
