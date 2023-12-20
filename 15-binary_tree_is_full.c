145dead209c88b917ef116771d92cefd44449a34
Your Name <you@example.com>
2023-12-20 13:49:57 -0800

    checks if current node is leaf, and if not, checks subtree to determine fullness

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,3 +10,16 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	/* current node is leaf node and, thus, is full */
+
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	/* if current node has left and right child, checks to see if subtree full */
+	return (0);
+	/* returns 0 if subtree not full */
+}
