fc27fa7637265ca5f179a98104cb14a85679c6af
Your Name <you@example.com>
2023-12-20 13:10:22 -0800

    counts nodes with at least one child recursively

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,3 +9,11 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left != NULL || tree->right != NULL)
+		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1); /* if current node has child, increments count */
+
+	return (0); /* if current node has no child, returns 0 */
+}
