43038c4f5c6315dd2cc22ff71ca9685c8c5a4683
Your Name <you@example.com>
2023-12-20 14:02:59 -0800

    checks for siblings of current node

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -5,8 +5,22 @@
  * binary_tree_sibling - function that finds the sibling of a node
  * Sibling - node that shares the same parent
  * @node: pointer to the node to find the sibling
- * 
+ *
  * Return: pointer to sibling node or NULL if node has no sibling
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node->parent->left == NULL | node->parent->right == NULL)
+		return (NULL);
+
+	if (node == node->parent->left) /* checks if node is left child of parent */
+		return (node->parent->right); /* if true, returns pointer to sibling */
+
+	return (node->parent->left);
+	/* if false, node is right child -> returns left sibling */
+}
+
+
